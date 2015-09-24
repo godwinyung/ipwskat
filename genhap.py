4180fbfc71f325a3e12c1c23e0d86d2390b30007
godwinyung <godwin.y.yung@gmail.com>
2015-09-24 16:50:45 -0400

    new functionality: generate specific haplotypes

--- genhap.py
+++ genhap.py
@@ -2,52 +2,48 @@ import sys
 import os.path
 
 # given r, create haplotype for rth region
-def genhap(r):
+def genhap(infile1, infile2, folder, minr, maxr):
   
-  regions = open('/n/home01/godwin/Research/ipwskat/data/cosi/10Kregions.txt','r')
-  i = 1
+  regions = open(infile2,'r')
+  r = 1
   for rline in regions:
-    
-    if (i == r):
+    outfile = folder + '/haplotypes' + str(r) + '.txt'
+    if (r >= minr and not os.path.isfile(outfile)):
       
-      filename = '/n/home01/godwin/Research/ipwskat/data/cosi/10Khaplotypes/haplotypes'+str(r)+'.txt'
-      if (not os.path.isfile(filename)):
-        # get min and max index for variants within the chosen subregion
-        [mini, maxi] = rline.rstrip().split()
-        [mini, maxi] = map(int, [mini, maxi])
-        
-        # create haplotype matrix
-        haplotypes = open(filename,'w')
-        cosi = open('/n/home01/godwin/Research/ipwskat/data/cosi/out.hap-1','r')
-        for cline in cosi:
-          hap = cline.rstrip().split()[mini+1:maxi+2] # restrict each line to only variants within the chosen subregion
-          for item in hap:
-            haplotypes.write(str(2-int(item))+'\t')
-          
-          haplotypes.write('\n')
-        
-        haplotypes.close()
+      # get min and max index for variants within the chosen subregion
+      [mini, maxi] = rline.rstrip().split()
+      [mini, maxi] = map(int, [mini, maxi])
       
-    i = i + 1
+      # create haplotype matrix
+      out = open(outfile,'w')
+      haplotypes = open(infile1,'r')
+      for haplotype in haplotypes:
+          rhap = haplotype.rstrip().split()[mini+1:maxi+2] # restrict each line (haplotype) to only variants within the chosen subregion
+          for item in rhap:
+            out.write(str(2-int(item))+'\t')
+          out.write('\n')
+      out.close()
+      haplotypes.close()
+      
+    r = r + 1
+    if (r > maxr):
+      break
+  
+  regions.close()
 
 
 def main():
   args = sys.argv[1:]
-  
-  taskID1   = int(args[0])
-  taskID2   = int(args[1])
-  njobs     = int(args[2])
-  interval  = 1000/njobs
-  rmin      = 10000/njobs*(taskID1-1)+interval*(taskID2-1)+1
-  rmax      = 10000/njobs*(taskID1-1)+interval*(taskID2-1)+interval
-
-  file = open('../output/done.txt','r')
-  done = file.readline().split()
-  file.close()
-  
-  for r in range(rmin,rmax+1):
-      if (str(r) not in done):
-          genhap(r)
+  filename_hap = args[0] # file containing haplotypes across entire 1Mb region
+  filename_reg = args[1] # file containing chromosomal regions (beginning and end bp) to restrict the haplotypes to
+  foldname_out = args[2] # folder where restricted haplotypes will be saved to
+  if len(args) == 5:
+    minr = int(args[3])
+    maxr = int(args[4])
+  else:
+    minr = 1
+    maxr = float("inf")
+  genhap(filename_hap, filename_reg, foldname_out, minr, maxr)
 
 
 if __name__ == '__main__':
