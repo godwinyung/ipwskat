8cbb79da47ec343aa5463d0463e4d64d4e05b75b
godwinyung <godwin.y.yung@gmail.com>
2015-09-25 09:50:25 -0400

    Modify: generate 10 haplotypes at a time

--- genhap.py
+++ genhap.py
@@ -1,14 +1,18 @@
 import sys
 import os.path
 
+# Let S={{1,...,10},{11,...20},...}. Given r, return the index of the subset in S containing r. 
+def mapS(r):
+  return (r-1)/10+1
+
 # given r, create haplotype for rth region
-def genhap(infile1, infile2, folder, minr, maxr):
+def genhap(infile1, infile2, folder, min10r, max10r):
   
   regions = open(infile2,'r')
   r = 1
   for rline in regions:
     outfile = folder + '/haplotypes' + str(r) + '.txt'
-    if (r >= minr and not os.path.isfile(outfile)):
+    if (mapS(r) >= min10r and not os.path.isfile(outfile)):
       
       # get min and max index for variants within the chosen subregion
       [mini, maxi] = rline.rstrip().split()
@@ -26,7 +30,7 @@ def genhap(infile1, infile2, folder, minr, maxr):
       haplotypes.close()
       
     r = r + 1
-    if (r > maxr):
+    if (mapS(r) > max10r):
       break
   
   regions.close()
@@ -38,12 +42,12 @@ def main():
   filename_reg = args[1] # file containing chromosomal regions (beginning and end bp) to restrict the haplotypes to
   foldname_out = args[2] # folder where restricted haplotypes will be saved to
   if len(args) == 5:
-    minr = int(args[3])
-    maxr = int(args[4])
+    min10r = int(args[3])
+    max10r = int(args[4])
   else:
-    minr = 1
-    maxr = float("inf")
-  genhap(filename_hap, filename_reg, foldname_out, minr, maxr)
+    min10r = 1
+    max10r = float("inf")
+  genhap(filename_hap, filename_reg, foldname_out, min10r, max10r)
 
 
 if __name__ == '__main__':
