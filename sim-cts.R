939661ac9b8c6d86e6faea5aec3cbddf091eb3d2
godwinyung <godwin.y.yung@gmail.com>
2016-03-25 13:44:49 -0400

    include non-rare variants in analysis

--- sim-cts.R
+++ sim-cts.R
@@ -40,10 +40,10 @@ sim.cts <- function(c.Y = 0, c.D = 0, beta.Y = 0, kappa = 0.1, link = "logit", M
           }
      } else {
           p.values        <- data.frame(matrix(NA,nrow=(R.max-R.min+1)*S,ncol=2+6*(1+n.corr)))
-          characteristics <- data.frame(matrix(0,nrow=(R.max-R.min+1)*S,ncol=11))
+          characteristics <- data.frame(matrix(0,nrow=(R.max-R.min+1)*S,ncol=13))
           
           names(p.values) <- c("r", "s", paste("r",paste(c(seq(n.corr),"opt"),".ctrl",sep=""),sep=""), paste("r",paste(c(seq(n.corr),"opt"),".case",sep=""),sep=""), paste("r",paste(c(seq(n.corr),"opt"),".naive",sep=""),sep=""), paste("r",paste(c(seq(n.corr),"opt"),".joint",sep=""),sep=""), paste("r",paste(c(seq(n.corr),"opt"),".ipw",sep=""),sep=""), paste("r",paste(c(seq(n.corr),"opt"),".aipw",sep=""),sep=""))
-          names(characteristics) <- c("kappa", "n.variants", "n.variants.03", "n.variants.01", "n.variants.001", "n.ovariants.cc", "n.ovariants.case", "n.ovariants.ctrl", "p.cc", "p.case", "p.ctrl")
+          names(characteristics) <- c("kappa", "n.variants", "n.variants.03", "n.variants.01", "n.variants.001",  "n.ovariants", "n.ovariants.03", "n.ovariants.01", "n.variants.001", "n.cvariants", "n.cvariants.03", "n.cvariants.01", "n.cvariants.001")
           
           p.values$r <- rep(seq(R.min,R.max),each=S)
           p.values$s <- rep(seq(1,S),R.max-R.min+1)
@@ -62,43 +62,49 @@ sim.cts <- function(c.Y = 0, c.D = 0, beta.Y = 0, kappa = 0.1, link = "logit", M
           haplotype <- data.matrix(read.table(fname))
           haplotype <- subset(haplotype,rowSums(is.na(haplotype))==0)
           n.hap     <- dim(haplotype)[1]
+          n.variants<- dim(haplotype)[2]
           
-          # variants in subregion
-          temp <- SNPInfo[regions[R.min+r-1,1]:regions[R.min+r-1,2],]
-          results$characteristics[((r-1)*S+1):(r*S),]$n.variants       <- dim(temp)[1]
-          results$characteristics[((r-1)*S+1):(r*S),]$n.variants.03    <- sum(temp$FREQ1 < .03)
-          results$characteristics[((r-1)*S+1):(r*S),]$n.variants.01    <- sum(temp$FREQ1 < .01)
-          results$characteristics[((r-1)*S+1):(r*S),]$n.variants.001   <- sum(temp$FREQ1 < .001)
+          # statistics of variants in subregion
+          info  <- SNPInfo[regions[R.min+r-1,1]:regions[R.min+r-1,2],]
+          FREQ  <- info$FREQ1 # allele frequencies
+          cFREQ <- unlist(lapply(FREQ,fun(x) min(x,1-x))) # minor allele frequencies
+          results$characteristics[((r-1)*S+1):(r*S),]$n.variants       <- n.variants
+          results$characteristics[((r-1)*S+1):(r*S),]$n.variants.03    <- sum(cFREQ < .03)
+          results$characteristics[((r-1)*S+1):(r*S),]$n.variants.01    <- sum(cFREQ < .01)
+          results$characteristics[((r-1)*S+1):(r*S),]$n.variants.001   <- sum(cFREQ < .001)
           
           # rare variants in subregion
-          rvariants      <- which(temp$FREQ1 < MAF.threshold)              # index wrt variants in subregion
-          n.rvariants    <- length(rvariants)                              # number of rare variants
-          info           <- temp[rvariants,]                               # information on rare variants
+          rvariants      <- which(cFREQ < MAF.threshold)   # index wrt variants in subregion
+          n.rvariants    <- length(rvariants)              # number of rare variants
+#           info           <- temp[rvariants,]                               # information on rare variants
           
-          # Generate population genotype matrix vG (variants) and G (rare variants)
+          # generate population genotype matrix G
           hap1 <- sample(n.hap, N, replace=TRUE)
           hap2 <- sample(n.hap, N, replace=TRUE)
-          vG   <- haplotype[hap1,] + haplotype[hap2,]
-          G    <- vG[,rvariants]
+          G    <- haplotype[hap1,] + haplotype[hap2,]
+          G[,FREQ>0.5] <- 2-G[,FREQ>0.5]
+#           G    <- vG[,rvariants]
           
-          cvariants      <- sample(n.rvariants, n.rvariants*prop.D)        # randomly select a set of rare variants to be causally associated with D
+          # randomly select a set of rare variants to be causally associated with D
+          cvariants <- sample(rvariants, n.rvariants*prop.D)
+          beta.G <- matrix(0, nrow=n.variants, ncol=1)
           if (c.D != 0) { # randomly select a subset of the causal variants to have positive association with D
-               positive       <- sample(cvariants, length(cvariants)*prop.pos.D)          # index wrt rvariants
-               pcvariants     <- seq(1:n.rvariants) %in% positive                         # causal variant w/ positive association (T/F)
-               ncvariants     <- seq(1:n.rvariants) %in% setdiff(cvariants, positive)     # causal variant w/ negative association (T/F)
-               beta.G         <- matrix((-1*pcvariants + ncvariants) * c.D * log10(info$FREQ1) / 2, ncol=1)
-          } else {
-               beta.G         <- matrix(0, nrow=n.rvariants, ncol=1)
+               positive       <- sample(cvariants, length(cvariants)*prop.pos.D)
+               pcvariants     <- cvariants[cvariants %in% positive]  # causal variant w/ positive association (T/F)
+               ncvariants     <- cvariants[!cvariants %in% positive] # causal variant w/ negative association (T/F)
+               beta.G[pcvariants,] <- -1*c.D*log10(cFREQ1[pcvariants])/2
+               beta.G[ncvariants,] <- c.D*log10(cFREQ1[pcvariants])/2
           }
           
-          cvariants      <- sample(n.rvariants, n.rvariants*prop.Y)        # randomly select a set of rare variants to be causally associated with Y
+          # randomly select a set of rare variants to be causally associated with Y
+          cvariants <- sample(rvariants, n.rvariants*prop.Y)
+          alpha.G <- matrix(0, nrow=n.variants, ncol=1)
           if (c.Y != 0) { # randomly select a subset of the causal variants to have positive association with Y
-               positive       <- sample(cvariants, length(cvariants)*prop.pos.Y)          # index wrt rvariants
-               pcvariants     <- seq(1:n.rvariants) %in% positive                         # causal variant w/ positive association (T/F)
-               ncvariants     <- seq(1:n.rvariants) %in% setdiff(cvariants, positive)     # causal variant w/ negative association (T/F)
-               alpha.G        <- matrix((-1*pcvariants + ncvariants) * c.Y * log10(info$FREQ1) / 2, ncol=1)
-          } else {
-               alpha.G        <- matrix(0, nrow=n.rvariants, ncol=1)
+               positive       <- sample(cvariants, length(cvariants)*prop.pos.Y)          
+               pcvariants     <- cvariants[cvariants %in% positive]  # causal variant w/ positive association (T/F)
+               ncvariants     <- cvariants[!cvariants %in% positive] # causal variant w/ negative association (T/F)
+               alpha.G[pcvariants,] <- -1*c.Y*log10(cFREQ1[pcvariants])/2
+               alpha.G[ncvariants,] <- c.Y*log10(cFREQ1[pcvariants])/2
           }
           
           while (s <= S) {
@@ -127,24 +133,26 @@ sim.cts <- function(c.Y = 0, c.D = 0, beta.Y = 0, kappa = 0.1, link = "logit", M
                     cases     <- sample(which(D==1), n1)
                     controls  <- sample(which(D==0), n1)
                     
-                    results$characteristics[(r-1)*S+s,]$kappa            <- mean(D)                                             # disease prevalence
-                    results$characteristics[(r-1)*S+s,]$n.ovariants.cc   <- sum(colSums(vG[c(cases,controls),])>0)              # number of variants observed by cases and ctrls
-                    results$characteristics[(r-1)*S+s,]$n.ovariants.case <- sum(colSums(vG[cases,])>0)                          
-                    results$characteristics[(r-1)*S+s,]$n.ovariants.ctrl <- sum(colSums(vG[controls,])>0)
-                    results$characteristics[(r-1)*S+s,]$p.cc             <- sum(colSums(G[c(cases,controls),cvariants])>0)      # number of causal variants observed by cases and ctrls
-                    results$characteristics[(r-1)*S+s,]$p.case           <- sum(colSums(G[cases,cvariants])>0)
-                    results$characteristics[(r-1)*S+s,]$p.ctrl           <- sum(colSums(G[controls,cvariants])>0)
-                    
-                    MAF  <- info$FREQ1
+                    results$characteristics[(r-1)*S+s,]$kappa            <- mean(D)                                           # disease prevalence
+                    temp <- colSums(G[c(cases,controls),])>0
+                    results$characteristics[(r-1)*S+s,]$n.ovariants      <- sum(temp)
+                    results$characteristics[(r-1)*S+s,]$n.ovariants.03   <- sum(temp & cFREQ < 0.03)
+                    results$characteristics[(r-1)*S+s,]$n.ovariants.01   <- sum(temp & cFREQ < 0.01)
+                    results$characteristics[(r-1)*S+s,]$n.ovariants.001  <- sum(temp & cFREQ < 0.001)
+                    temp <- colSums(G[c(cases,controls),cvariants]>0)
+                    results$characteristics[(r-1)*S+s,]$n.cvariants      <- sum(temp)
+                    results$characteristics[(r-1)*S+s,]$n.cvariants.03   <- sum(temp & cFREQ[cvariants] < 0.03)
+                    results$characteristics[(r-1)*S+s,]$n.cvariants.01   <- sum(temp & cFREQ[cvariants] < 0.01)
+                    results$characteristics[(r-1)*S+s,]$n.cvariants.001  <- sum(temp & cFREQ[cvariants] < 0.001)
                     
                     # control-only SKAT-O
                     Y1   <- Y[controls] 
                     X1   <- X[controls, ]
                     ovariants <- which( colSums( G[controls, ] ) > 0 )
                     G1   <- G[controls, ovariants]
-                    MAF1 <- MAF[ovariants]
+                    MAF  <- cFREQ[ovariants]
                     obj  <- SKAT_Null_Model(Y1~X1)
-                    out  <- try(SKAT(G1, obj, weights=dbeta(MAF1,1,25), r.corr=r.corr))
+                    out  <- try(SKAT(G1, obj, weights=dbeta(MAF,1,25), r.corr=r.corr))
                     if ( class(out) != "try-error" ) {
                       results$p.values[(r-1)*S+s,3:(3+n.corr)] <- c(out$param$p.val.each, out$p.value)
                     }
@@ -154,9 +162,9 @@ sim.cts <- function(c.Y = 0, c.D = 0, beta.Y = 0, kappa = 0.1, link = "logit", M
                     X1   <- X[cases, ]
                     ovariants <- which( colSums( G[cases, ] ) > 0 )
                     G1   <- G[cases, ovariants]
-                    MAF1 <- MAF[ovariants]
+                    MAF  <- cFREQ[ovariants]
                     obj  <- SKAT_Null_Model(Y1~X1)
-                    out  <- try(SKAT(G1, obj, weights=dbeta(MAF1,1,25), r.corr=r.corr))
+                    out  <- try(SKAT(G1, obj, weights=dbeta(MAF,1,25), r.corr=r.corr))
                     if ( class(out) != "try-error" ) {
                       results$p.values[(r-1)*S+s,(4+n.corr):(4+2*n.corr)] <- c(out$param$p.val.each, out$p.value)
                     }
@@ -166,9 +174,9 @@ sim.cts <- function(c.Y = 0, c.D = 0, beta.Y = 0, kappa = 0.1, link = "logit", M
                     X1   <- X[c(cases,controls), ]
                     ovariants <- which( colSums( G[c(cases,controls), ] ) > 0 )
                     G1   <- G[c(cases,controls), ovariants]
-                    MAF1 <- MAF[ovariants]
+                    MAF  <- cFREQ[ovariants]
                     obj  <- SKAT_Null_Model(Y1~X1)
-                    out  <- try(SKAT(G1, obj, weights=dbeta(MAF1,1,25), r.corr=r.corr))
+                    out  <- try(SKAT(G1, obj, weights=dbeta(MAF,1,25), r.corr=r.corr))
                     if ( class(out) != "try-error" ) {
                       results$p.values[(r-1)*S+s,(5+2*n.corr):(5+3*n.corr)] <- c(out$param$p.val.each, out$p.value)
                     }
@@ -176,7 +184,7 @@ sim.cts <- function(c.Y = 0, c.D = 0, beta.Y = 0, kappa = 0.1, link = "logit", M
                     # joint SKAT-O
                     X1   <- cbind(X,D)[c(cases,controls), ]
                     obj  <- SKAT_Null_Model(Y1~X1)
-                    out  <- try(SKAT(G1, obj, weights=dbeta(MAF1,1,25), r.corr=r.corr))
+                    out  <- try(SKAT(G1, obj, weights=dbeta(MAF,1,25), r.corr=r.corr))
                     if ( class(out) != "try-error" ) {
                       results$p.values[(r-1)*S+s,(6+3*n.corr):(6+4*n.corr)] <- c(out$param$p.val.each, out$p.value)
                     }
@@ -186,8 +194,8 @@ sim.cts <- function(c.Y = 0, c.D = 0, beta.Y = 0, kappa = 0.1, link = "logit", M
                     D1   <- D[c(cases,controls)]
                     weights.ipw <- D1*2*mean(D) + (1-D1)*2*mean(1-D)
                     obj <- IPWSKAT_Null_Model(Y1~X1,weights.ipw=weights.ipw,ccstatus=D1,n.resampling=n.resampling)
-                    opt.asy <- try(IPWSKAT(G1, obj, weights.snp=dbeta(MAF1,1,25), r.corr=r.corr, adjustment=F))
-                    opt.adj <- try(IPWSKAT(G1, obj, weights.snp=dbeta(MAF1,1,25), r.corr=r.corr, adjustment=T))
+                    opt.asy <- try(IPWSKAT(G1, obj, weights.snp=dbeta(MAF,1,25), r.corr=r.corr, adjustment=F))
+                    opt.adj <- try(IPWSKAT(G1, obj, weights.snp=dbeta(MAF,1,25), r.corr=r.corr, adjustment=T))
                     if (inherits(opt.asy,"try-error") | inherits(opt.adj,"try-error")) {
                          s = s - 1
                     } else {
